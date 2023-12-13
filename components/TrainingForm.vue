9df30cfb52b851eebec9ee39379413edb45152e5
logosww <2711018508@qq.com>
2023-12-13 21:30:40 +0800

    feat: date form item till present use switch comp

--- components/TrainingForm.vue
+++ components/TrainingForm.vue
@@ -5,7 +5,7 @@
     </el-form-item>
     <el-form-item label="结束日期" prop="endDate">
       <el-date-picker v-model="form.endDate" type="month" value-format="YYYY-MM" :disabled="form.endDate === '至今'" />
-      <el-checkbox style="margin-left: 16px;" v-model="form.endDate" true-label="至今">至今</el-checkbox>
+      <el-switch style="margin-left: 16px;" v-model="form.endDate" active-text="至今" active-value="至今" />
     </el-form-item>
     <el-form-item label="培训类别" prop="trainType">
       <Select v-model="form.trainType" select-target="培训学习类别" />
